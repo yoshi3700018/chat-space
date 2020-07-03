1674fa0f603caf42def4e2a10f47ed418cae0ed0
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-07-03 18:41:42 +0900

    Before_installing_pry
    Before_installing_pry

---
+++ app/models/group.rb
@@ -0,0 +1,6 @@
+# アソシエーションを設定する。
+class Group < ApplicationRecord
+  has_many :group_users
+  has_many :users, through: :group_users #多対多記述。has_many引数に「アソシ組みたいテーブル名」を、:throughのバリューに「中間テーブル名」を指定。これで「group.users」といった呼出ができる。
+  validates :name, presence: true, uniqueness: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # アソシエーション設定
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable #?
   
-  validates :name, presence: true, uniqueness: true       
+  validates :name, presence: true, uniqueness: true    #?
+  
+  has_many :group_users
+  has_many :groups, through: :group_users #?
   
 end
