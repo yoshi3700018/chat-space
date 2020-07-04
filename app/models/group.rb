4afce13dde23964ca71a01606d3c896a705a6710
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-07-04 18:16:32 +0900

    chat__form
    chat__form

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users #多対多記述。has_many引数に「アソシ組みたいテーブル名」を、:throughのバリューに「中間テーブル名」を指定。これで「group.users」といった呼出ができる。
+  has_many :messages
   validates :name, presence: true, uniqueness: true
 end
