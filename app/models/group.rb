4df7821bcc69dcec782bb8b83b0ccd602d3feaab
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-07-05 11:59:33 +0900

    chatmain
    chatmain

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,16 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users #多対多記述。has_many引数に「アソシ組みたいテーブル名」を、:throughのバリューに「中間テーブル名」を指定。これで「group.users」といった呼出ができる。
   has_many :messages
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
