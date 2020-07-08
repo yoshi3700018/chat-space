3f246ecaac78212fb10434cccf5d4a03870feb6e
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-07-08 17:04:38 +0900

    10. Try to scroll to the bottom of the message screen when you send a message.
    10. Try to scroll to the bottom of the message screen when you send a message.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,41 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="chat__main__name2">
+        <p class="m-name">
+          ${message.user_name}
+        </p>
+        <p class="date">
+          ${message.created_at}
+        </p>
+        </div>
+        <p class="message">
+          ${message.content}
+        </p>
+        <div class="chat__main__message">
+          <p class="message">
+          <img class="Message__image" src="${message.image}">
+          </p>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="chat__main__name2">
+      <p class="m-name">
+        ${message.user_name}
+      </p>
+      <p class="date">
+        ${message.created_at}
+      </p>
+      </div>
+      <p class="message">
+      ${message.content}
+      </p>`
+      return html;
+    };
+  }
+
   $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +47,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      let html = buildHTML(data);
+      $('.chat__main').append(html);      
+      $('form')[0].reset();
     })
   });
 });
