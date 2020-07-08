25a876d5db3fd9a011692b0c16f1d143e0d64e45
yoshi3700018 <yoshiyuki.hashimoto.0@gmail.com>
2020-07-08 11:16:53 +0900

    6. save the message with messages#create
    Save the message with messages#create and use respond_to Let's separate the process for HTML and JSON

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(function(){
   $('.Form').on('submit', function(e){
-    e.preventDefault()
-    console.log("hoge")
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
